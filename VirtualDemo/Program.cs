4073417be10a72b05794d591a622484c5f9c3689
alexis <alexisd.horton@gmail.com>
2021-12-06 09:29:17 -0500

    Initial Commit

---
+++ VirtualDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
