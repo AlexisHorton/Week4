969e2bcdb0e4ee06500e95e46ee2ffdc2bad1fed
alexis <alexisd.horton@gmail.com>
2021-12-12 14:59:03 -0500

    New + Lab

---
+++ InterfaceLab/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InterfaceLab
+{
+    interface ICountable
+    {
+        void IncrementCount();
+        void ResetCount();
+        int GetCount();
+        String GetCountString();
+    }
+
+    class Alligator : ICountable
+    {
+        public int Count;
+
+        public int GetCount()
+        {
+            return Count;
+        }
+
+        public string GetCountString()
+        {
+            return Count.ToString();
+        }
+
+        public void IncrementCount()
+        {
+           Count++;
+        }
+
+        public void ResetCount()
+        {
+            Count = 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- PropertiesDemo/Program.cs
+++ PropertiesDemo/Program.cs
@@ -31,13 +31,54 @@ namespace PropertiesDemo
         }
     }
 
+    class RectWithProps
+    {
+        private double pLength;
+        private double pWidth;
+
+        public double Length
+        {
+            get
+            {
+                return pLength;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return pWidth;
+            }
+        }
+        public RectWithProps(double Length, double Width)
+        {
+            pLength = Length;
+            pWidth = Width;
+        }
+
+        public double GetArea()
+        {
+            return pLength * pWidth;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Rectangle r1 = new Rectangle(3.5, 2.6);
+
             Console.WriteLine($"Rectangle has Length {r1.GetLength()} and Width {r1.GetWidth()}");
-            Console.WriteLine(r1.GetArea());
+
+            Console.WriteLine($"The area is: {r1.GetArea()}");
+
+            Console.WriteLine();
+
+            RectWithProps r2 = new RectWithProps(4.2, 5.5);
+            Console.WriteLine(r2.Length);
+            Console.WriteLine(r2.Width);
+
         }
     }
 }
